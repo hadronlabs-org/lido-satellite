de0fdf739d169137986e710d85cf4eb6335fe81e
Murad Karammaev <muradkarammaev@gmail.com>
2023-08-18 18:34:39 +0300

    feat: simplify GMP helper

--- contracts/solidity/gmp-helper/gmp-helper.sol
+++ contracts/solidity/gmp-helper/gmp-helper.sol
@@ -9,23 +9,20 @@ contract GmpHelper {
     IAxelarGasService public immutable gasService;
     IAxelarGateway public immutable gateway;
     IERC20 public immutable wstEth;
-    string public wstEthSymbol;
-    string public destinationChain;
     string public lidoSatellite;
+
+    string public constant DESTINATION_CHAIN = "neutron";
+    string public constant WSTETH_SYMBOL = "wstETH";
 
     constructor(
         address axelarGateway_,
         address axelarGasReceiver_,
         address wstEth_,
-        string memory axelarSymbolForWstEth_,
-        string memory destinationChain_,
         string memory lidoSatellite_
     ) {
         gasService = IAxelarGasService(axelarGasReceiver_);
         gateway = IAxelarGateway(axelarGateway_);
         wstEth = IERC20(wstEth_);
-        wstEthSymbol = axelarSymbolForWstEth_;
-        destinationChain = destinationChain_;
         lidoSatellite = lidoSatellite_;
     }
 
@@ -33,8 +30,6 @@ contract GmpHelper {
         string calldata receiver,
         uint256 amount
     ) external payable {
-        require(msg.value >= 1000000 gwei);
-
         // 1. withdraw wstETH from caller and approve it for Axelar Gateway.
         // Gateway will attempt to transfer funds from address(this), hence we
         // are forced to withdraw them from caller account first.
@@ -47,20 +42,20 @@ contract GmpHelper {
         // 3. Pay for gas
         gasService.payNativeGasForContractCallWithToken{value: msg.value}(
             address(this),
-            destinationChain,
+            DESTINATION_CHAIN,
             lidoSatellite,
             payload,
-            wstEthSymbol,
+            WSTETH_SYMBOL,
             amount,
             msg.sender
         );
 
         // 4. Make GMP call
         gateway.callContractWithToken(
-            destinationChain,
+            DESTINATION_CHAIN,
             lidoSatellite,
             payload,
-            wstEthSymbol,
+            WSTETH_SYMBOL,
             amount
         );
     }
