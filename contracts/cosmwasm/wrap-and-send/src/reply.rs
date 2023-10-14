b861ca401c260fa0e6cc613f6c7d78937231a93d
Murad Karammaev <muradkarammaev@gmail.com>
2023-10-14 11:18:08 +0300

    wip: refactor reply

--- contracts/cosmwasm/wrap-and-send/src/reply.rs
+++ contracts/cosmwasm/wrap-and-send/src/reply.rs
@@ -13,26 +13,21 @@ pub fn reply_wrap_callback(
     _env: Env,
     result: SubMsgResult,
 ) -> ContractResult<Response<NeutronMsg>> {
-    EXECUTION_FLAG.remove(deps.storage);
+    if let SubMsgResult::Err(_) = result {
+        EXECUTION_FLAG.remove(deps.storage);
 
-    match result {
-        // I ignore this error string since I am not sure how to propogate it
-        // and inserting it into attributes doesn't sound right at all
-        SubMsgResult::Err(_e) => {
-            let refund_info = REFUND_INFO.load(deps.storage)?;
+        let refund_info = REFUND_INFO.load(deps.storage)?;
 
-            let send_msg: CosmosMsg<NeutronMsg> = BankMsg::Send {
-                to_address: refund_info.refund_address.into_string(),
-                amount: vec![refund_info.funds],
-            }
-            .into();
-
-            // TODO: attributes
-            Ok(Response::new().add_message(send_msg))
+        let send_msg: CosmosMsg<NeutronMsg> = BankMsg::Send {
+            to_address: refund_info.refund_address.into_string(),
+            amount: vec![refund_info.funds],
         }
-        // We don't need to do anything, our job is done
+        .into();
+
         // TODO: attributes
-        SubMsgResult::Ok(_response) => Ok(Response::new()),
+        Ok(Response::new().add_message(send_msg))
+    } else {
+        unreachable!("because we use `SubMsg::reply_on_error`")
     }
 }
 
@@ -41,30 +36,28 @@ pub(crate) fn reply_ibc_transfer(
     _env: Env,
     result: SubMsgResult,
 ) -> ContractResult<Response<NeutronMsg>> {
-    EXECUTION_FLAG.remove(deps.storage);
+    if let SubMsgResult::Ok(response) = result {
+        EXECUTION_FLAG.remove(deps.storage);
 
-    let context = IBC_TRANSFER_CONTEXT.load(deps.storage)?;
-    IBC_TRANSFER_CONTEXT.remove(deps.storage);
+        let context = IBC_TRANSFER_CONTEXT.load(deps.storage)?;
+        IBC_TRANSFER_CONTEXT.remove(deps.storage);
 
-    match result {
-        // we request reply on success, so we don't have to implement failure handling
-        SubMsgResult::Err(_e) => unimplemented!(),
-        SubMsgResult::Ok(response) => {
-            // IBC transfer module always sets reply data on success:
-            // https://github.com/neutron-org/neutron/blob/v1.0.4/x/transfer/keeper/keeper.go#L27-L62
-            // hence I can easily use `Option::unwrap()` here
-            let data = response.data.unwrap();
-            let response: MsgIbcTransferResponse = from_binary(&data)?;
-            IBC_TRANSFER_INFO.save(
-                deps.storage,
-                (response.sequence_id, &response.channel),
-                &context,
-            )?;
-            Ok(Response::new().add_attributes([
-                attr("subaction", "ibc_transfer"),
-                attr("sequence_id", response.sequence_id.to_string()),
-                attr("channel", response.channel),
-            ]))
-        }
+        // IBC transfer module always sets reply data on success:
+        // https://github.com/neutron-org/neutron/blob/v1.0.4/x/transfer/keeper/keeper.go#L27-L62
+        // hence I can easily use `Option::unwrap()` here
+        let data = response.data.unwrap();
+        let response: MsgIbcTransferResponse = from_binary(&data)?;
+        IBC_TRANSFER_INFO.save(
+            deps.storage,
+            (response.sequence_id, &response.channel),
+            &context,
+        )?;
+        Ok(Response::new().add_attributes([
+            attr("subaction", "ibc_transfer"),
+            attr("sequence_id", response.sequence_id.to_string()),
+            attr("channel", response.channel),
+        ]))
+    } else {
+        unreachable!("because we use `SubMsg::reply_on_success`")
     }
 }
